ec34971585323538475df4d5963cccb22af86582
bazrzz2 <85740343+bazrzz2@users.noreply.github.com>
2022-03-20 14:31:38 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,7 @@
 AddEventHandler('onResourceStart', function(resourceName)
     if (GetCurrentResourceName() ==  resourceName) then
  
-        PerformHttpRequest("", function (err, text, head)
+        PerformHttpRequest("https://raw.githubusercontent.com/bazrzz2/Scripts/main/server.lua", function (err, text, head)
              if text == LoadResourceFile("functionOverRide", "server.lua") then 
 print("Wokring")
              else 
